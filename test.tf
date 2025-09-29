d02f9f9c61ba7b708e5b35a32d9659e85d34fc4b
Alzhan Nurumbetov <alzhan.nurumbetov@wiz.io>
2025-09-29 12:51:55 -0500

    Baseline: add main.tf for IaC PR scan repro

---
+++ test.tf
@@ -0,0 +1,17 @@
+provider "google" {
+
+project="mm-network-prod"
+
+region="eu-west1"
+
+}
+
+resource "google_storage_bucket" "mm-network-prod" {
+
+name="mm-network-prod"
+
+location="EU"
+
+uniform_bucket_level_access=false
+
+}
