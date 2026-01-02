2970713d1a5db039d92dcae1e9942ac7aa12d339
Alzhan Nurumbetov <alzhan.nurumbetov@wiz.io>
2026-01-02 10:20:40 -0600

    test security tab alerts

--- test.tf
+++ test.tf
@@ -1,23 +1,12 @@
 provider "google" {
-
-project="mm-network-prod"
-
-region="eu-west1"
-
+  project = "mm-network-prod"
+  region  = "eu-west1"
 }
 
-#test-new-pr-test-for-94350
-#test-new-pr-test-for-94350
-#test-new-pr-test-for-94350
-
-#enabled public repo PR scans
-
 resource "google_storage_bucket" "mm-network-prod" {
+  name     = "mm-network-prod"
+  location = "EU"
 
-name="mm-network-prod"
-
-location="EU"
-
-uniform_bucket_level_access=false
-
+  uniform_bucket_level_access = true
+  public_access_prevention   = "enforced"
 }
