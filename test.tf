3a918989d95f32244bc99db314041437b756a692
Alzhan Nurumbetov <alzhan.nurumbetov@wiz.io>
2026-02-05 11:21:51 -0600

    PR test 4

--- test.tf
+++ test.tf
@@ -7,9 +7,6 @@ resource "google_storage_bucket" "mm-network-prod" {
   name     = "mm-network-prod"
   location = "EU"
 
-  uniform_bucket_level_access = true
-  public_access_prevention   = "enforced"
-
   versioning {
     enabled = true
   }
